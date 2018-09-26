667a73cac51beed50f42902475286535f058f4cf
JacobHermanni <hijakker123@hotmail.com>
2018-09-26 20:31:15 +0200

    Manual commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,159 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import currentWeekNumber from 'current-week-number';
+import { checkAuth, load, updateCell, loadClient } from './spreadsheet';
+import 'react-tippy/dist/tippy.css';
+import { Tooltip } from 'react-tippy';
 import { secretprint, initConfig, config, API } from './printsecret';
 
 
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      days: [],
+      random: {},
+      authors: [],
+      author: '',
+      order: 'date',
+      authenticated: false,
+      uge: currentWeekNumber()
+    }
+  }
+
+  componentDidMount() {
+    window.gapi.load('client', () => {
+      loadClient(load(this.onLoad.bind(this)));
+      checkAuth(true, this.handleAuth.bind(this));
+    });
+  }
+
+  /**
+   * Check user authenification status and set app state accordingly
+   */
+  handleAuth(authResult) {
+    if (authResult && !authResult.error) {
+      this.setState({
+        authenticated: true
+      }, () => console.log("authenticated.."));
+      // following load might have to happen only after udate to sheet
+      load(this.onLoad.bind(this));
+    } else {
+      console.log("auth failed?");
+      this.setState({
+        authenticated: false
+      })
+    }
+  }
+
+  /**
+   * Once quotes have been loaded from the spreadsheet
+   */
+  onLoad(data, error) {
+    if (data) {
+      this.setState({
+        days: data
+      });
+    }
+    else {
+      this.setState({
+        error: error
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="app">
         {initConfig(secretprint)}
         {console.log("env", process.env.NODE_ENV)}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Tos get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>current env: {" " + process.env.NODE_ENV}</span>
-        <span> key: {" " + process.env.REACT_APP_KEY}</span>
+        <h1 className="brand">Madklub Quick</h1>
+        <button onClick={() => this.insertTest()} className="btn"> test insert</button>
         <button onClick={() => console.log("from app, printing config stuff: ", config, API)}>print secret</button>
+        {this.renderContent()}
       </div>
     );
   }
+
+  renderContent() {
+
+    if (this.state.days.length) {
+      console.log("if render 2");
+      return (
+        <div className="page">
+          <div className="nav-header">
+            <button className="btn" onClick={() => {
+              this.setState({ uge: this.state.uge - 1 });
+            }}>Forrige uge</button>
+
+            <span>Uge {this.state.uge}</span>
+
+            <button className="btn" onClick={() => {
+              this.setState({ uge: this.state.uge + 1 });
+            }}>Næste uge</button>
+          </div>
+
+          <div className="days">
+            {this.state.days.filter(day => Number(day.uge) === this.state.uge).map((day, i) => {
+              return (
+                <div key={i} className={`${day.kok ? "day-list_item" : "day-list_item-ingen-madklub"}`}>
+                  <h2 >{day.ugedag} {day.dato}</h2>
+                  <span className="madklub" title="">{day.kok || "Ingen madklub"}</span>
+                  {day.kok && (
+                    <Tooltip
+                      className="tilmeldte"
+                      title={day.tilmeldte && day.tilmeldte}
+                      position="right"
+                      trigger="click"
+                    >
+                      <span>
+                        {` - tilmeldte: ${day.tilmeldte && day.antalTilmeldte}`}
+                      </span>
+                    </Tooltip>
+                  )}
+                  {day.kok &&
+                    (<button className="btn" onClick={this.authenticate.bind(this)}>Tilmeld</button>)}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+    else if (this.state.error) {
+      console.log("if render 3");
+      console.log(this.state.error);
+      return (
+        <div> {this.state.error}</div>
+      );
+    }
+    else {
+      console.log("if render 4");
+      console.log("why is loader running? days lenght:", this.state.days);
+      return (
+        <div className="loader" />
+      );
+    }
+  }
+
+  /**
+   * Request Google authentification
+   */
+  authenticate(e) {
+    e.preventDefault();
+    if (this.state.authenticated) this.insertTest(); // add update sheet
+    else checkAuth(false, () => { this.handleAuth.bind(this); this.insertTest(); });
+  }
+
+  insertTest() {
+    updateCell('A', 2, "test", null, (error) => {
+      console.log("error while inserting", error);
+    })
+  }
+
+
 }
 
 export default App;
+
---
+++ src/spreadsheet.js
@@ -0,0 +1,85 @@
+import { config, API } from './printsecret';
+
+/**
+ * Get the user authentication status
+ */
+export function checkAuth(immediate, callback) {
+  window.gapi.auth.authorize({
+    'client_id': config.clientId,
+    'scope': "https://www.googleapis.com/auth/spreadsheets",
+    'immediate': immediate
+  }, callback);
+}
+
+export function loadClient(callback) {
+  window.gapi.client.setApiKey(API.key, callback);
+}
+
+/**
+ * Load the data from the spreadsheet
+ */
+export function load(callback) {
+  window.gapi.client.load('sheets', 'v4', () => {
+    window.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '166fSi7fmm7yeYSMVjvCrMp1DIoZLxn3vIKjQO9EjKCE',
+      range: 'ark2!A4:Z'
+    }).then((response) => {
+      const data = response.result.values || []
+
+      let days = data.map((day, i) => {
+        let row = i + 2, // Save row ID for later update
+          ugensKokke = day[0],
+          uge = day[1],
+          dato = day[2],
+          ugedag = day[3],
+          kok = day[4],
+          antalTilmeldte = day[8],
+          tilmeldte = []
+
+        for (let index = 11; index < day.length; index++) {
+          if (day[index] !== "") {
+            const tilmeldtesNr = index - 11 + 317;
+            if (tilmeldtesNr === Number(day[4])) {
+              tilmeldte.push(`${tilmeldtesNr} (${day[index]})`);
+            }
+            else if (Number(day[index]) > 1) {
+              tilmeldte.push(`${tilmeldtesNr} (${day[index] - 1})`);
+            }
+            else tilmeldte.push(tilmeldtesNr);
+          }
+        }
+
+        if (!tilmeldte.find(nr => Number(nr) === Number(day[4]))) tilmeldte.push(day[4]);
+
+        return {
+          row,
+          ugensKokke,
+          uge,
+          dato,
+          ugedag,
+          kok,
+          antalTilmeldte,
+          tilmeldte
+        }
+      });
+
+      callback(
+        days
+      );
+    }, (response) => {
+      callback(false, response.result.error);
+    });
+  });
+}
+
+/**
+ * Update a single cell value
+ */
+export function updateCell(column, row, value, successCallback, errorCallback) {
+  window.gapi.client.sheets.spreadsheets.values.update({
+    spreadsheetId: '166fSi7fmm7yeYSMVjvCrMp1DIoZLxn3vIKjQO9EjKCE',
+    range: 'ark2!' + column + row,
+    valueInputOption: 'USER_ENTERED',
+    values: [[value]]
+  }).then(successCallback, errorCallback);
+}
