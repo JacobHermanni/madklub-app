6aebfbda42b9685d3fb56b97488bcc110084493c
Kingo <philipandreaskingo@hotmail.com>
2018-10-01 23:11:35 +0200

    Page is now updated when the user inputs values

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
 
   componentDidMount() {
     window.gapi.load('client', () => {
-      loadClient(load(this.onLoad.bind(this)));
+      loadClient(load(this.onLoad.bind(this), this.state.uge, new Date().getFullYear()));
       checkAuth(true, this.handleAuth.bind(this));
     });
     initConfig(secretprint);
@@ -194,10 +194,10 @@ class App extends Component {
   tilmeld(roomNr, participants, dato) {
     var date = new Date(new Date().getFullYear(), 0, (1 + (this.state.uge - 1) * 7));
     date.setDate(dato.split('.')[0]);
-    if (this.state.authenticated) tilmeld(roomNr, this.state.uge, date, participants, null, error => console.log("Error tilmelding", error));
+    if (this.state.authenticated) tilmeld(roomNr, this.state.uge, date, participants, () => loadClient(load(this.onLoad.bind(this), this.state.uge, new Date().getFullYear())), error => console.log("Error tilmelding", error));
     else checkAuth(false, (result) => {
       this.handleAuth(result);
-      tilmeld(roomNr, this.state.uge, date, participants, null, error => console.log("Error tilmelding", error));
+      tilmeld(roomNr, this.state.uge, date, participants, () => loadClient(load(this.onLoad.bind(this), this.state.uge, new Date().getFullYear())), error => console.log("Error tilmelding", error));
     });
   }
 
